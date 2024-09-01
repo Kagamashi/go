9514ed12ce6ae39c70daa641a578eadecf47fed7
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-09-01 19:18:05 +0200

    add constant example

--- examples/variables/constants.go
+++ examples/variables/constants.go
@@ -8,14 +8,20 @@ import (
 const s string = "constant"
 
 func constants() {
-	fmt.Println(s)
 
-	const n = 500000000
+	//*** CONSTANTS
+	fmt.Println(s) //constant
 
-	const d = 3e20 / n
-	fmt.Println(d)
+	const n = 500000000 //const statement can appear anywhere a var statement can
 
-	fmt.Println(int64(d))
+	const d = 3e20 / n //constants perform arimetrric with arbitraty precision
+	fmt.Println(d)     //6e+11
 
-	fmt.Println(math.Sin(n))
+	// numeric constant has no type until it's given one; for example by explicit conversion
+	fmt.Println(int64(d)) //600000000000
+
+	//a number can be given type by using it in a context that requires one such as variable assignment or functions call
+	fmt.Println(math.Sin(n)) //-0.20470407323754404
+	//math.Sin expects a float64
+
 }
