3e443cbcc42a3cff6b861fe5bd6a55b52ba3b30b
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-09-16 12:25:23 +0200

    Add examples for File Paths operations in Go

--- examples/file_io_operations/line_filters.go
+++ examples/file_io_operations/line_filters.go
@@ -34,11 +34,11 @@ func line_filters() {
 	}
 
 	/*
-	> echo 'hello' > /tmp/lines
-	> echo 'filter' >> /tmp/lines
-	> cat /tmp/lines | go run line_filters.go
-	HELLO
-	FILTER
+		> echo 'hello' > /tmp/lines
+		> echo 'filter' >> /tmp/lines
+		> cat /tmp/lines | go run line_filters.go
+		HELLO
+		FILTER
 	*/
-	
+
 }
