8552d30ad89f74e3511573d085cdd282c64eea27
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-01-14 13:23:56 +0100

    Added new file: tasks/LeetCode/

---
+++ tasks/LeetCode/two_sum.go
@@ -0,0 +1,18 @@
+// 1. Two Sum
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	mymap := make(map[int]int)
+
+	for i, num := range nums {
+			if j, ok := mymap[target-num]; ok {
+					return []int{j, i}
+			}
+			mymap[num] = i
+	}
+	return nil
+}
