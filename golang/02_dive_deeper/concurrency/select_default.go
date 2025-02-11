c3e1eb26eafee2239ec447ef79d1c6812a2d3ed9
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2025-02-11 16:55:09 +0100

    Modified: golang/02_dive_deeper/concurrency/select_default.go

--- golang/02_dive_deeper/concurrency/select_default.go
+++ golang/02_dive_deeper/concurrency/select_default.go
@@ -1,8 +1,8 @@
 package main
-
 import "fmt"
 
-/* SELECT DEFAULT
+/* 
+SELECT DEFAULT
 Basic sends and receives on channels are blocking.
 However, we can use select with a default clause to implement non-blocking sends, receives, and even non-blocking multi-way selects.
 */
